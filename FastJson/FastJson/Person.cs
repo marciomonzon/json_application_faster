f710466872ff9a3020acba75ac099978f38ce57d
marciomonzon <marcio.pcmonzon@gmail.com>
2022-10-22 14:46:24 -0300

    fast mode to serialize and deserialize

---
+++ FastJson/FastJson/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace FastJson
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ FastJson/FastJson/Program.cs
@@ -0,0 +1,20 @@
+﻿using FastJson;
+using System.Text.Json;
+
+var person = new Person
+{
+    FirstName = "Fulano",
+    LastName = "DaSilva"
+};
+
+// customize
+var option = new JsonSerializerOptions
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+};
+
+var json = JsonSerializer.Serialize(person, option);
+
+
+Console.WriteLine(json);
+Console.ReadKey();
